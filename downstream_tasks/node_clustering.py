f9dd7650f1a4e164679e89cb2bef97964027831e
esprit1995 <mark.sandal@gmail.com>
2021-02-01 09:05:03 +0000

    added: kmeans is run 10 times instead of 1

--- downstream_tasks/node_clustering.py
+++ downstream_tasks/node_clustering.py
@@ -7,6 +7,7 @@ from sklearn.metrics.cluster import adjusted_rand_score
 from termcolor import cprint
 
 from datasets import IMDB_ACM_DBLP, ACM_HAN
+from statistics import mean
 
 
 def evaluate_clustering(args, node_embeddings: torch.tensor):
@@ -32,12 +33,13 @@ def evaluate_clustering(args, node_embeddings: torch.tensor):
     return kmeans_node_clustering(embs, labels)
 
 
-def kmeans_node_clustering(node_embeddings: torch.tensor, true_labels: torch.tensor) -> tuple:
+def kmeans_node_clustering(node_embeddings: torch.tensor, true_labels: torch.tensor, runs: int = 10) -> tuple:
     """
     perform k-means clustering on given node embeddings and evaluate it using NMI and ARI scores
     number of clusters is inferred from the number of different labels
     :param node_embeddings: node embeddings in torch.tensor format, shape = [n_nodes, n_features]
     :param true_labels: true labels of the node embeddings, shape = [n_nodes,]
+    :param runs: since kmeans is dependent on the center initialization, algo is rerun __runs__ times
     :return: tuple(NMI, ARI)
     """
     emb = node_embeddings.numpy()
@@ -45,14 +47,20 @@ def kmeans_node_clustering(node_embeddings: torch.tensor, true_labels: torch.ten
     n_clusters = np.unique(labels).size
 
     # fix random state for reproducible results
-    kmeans_labels = KMeans(n_clusters=n_clusters, random_state=0).fit(emb).labels_
-    NMI = normalized_mutual_info_score(kmeans_labels, labels)
-    ARI = adjusted_rand_score(kmeans_labels, labels)
+    random_states = list(range(runs))
+    nmis = list()
+    aris = list()
+    for rs in random_states:
+        kmeans_labels = KMeans(n_clusters=n_clusters, random_state=rs).fit(emb).labels_
+        NMI = normalized_mutual_info_score(kmeans_labels, labels)
+        ARI = adjusted_rand_score(kmeans_labels, labels)
+        nmis.append(NMI)
+        aris.append(ARI)
 
     print()
     cprint("--------------------------", color='blue')
-    cprint("Clustering task, NMI:  " + str(NMI), color='blue')
-    cprint("Clustering task, ARI:  " + str(ARI), color='blue')
+    cprint("Clustering task, NMI:  " + str(mean(nmis)), color='blue')
+    cprint("Clustering task, ARI:  " + str(mean(aris)), color='blue')
     cprint("--------------------------", color='blue')
     print()
-    return NMI, ARI
+    return mean(nmis), mean(aris)
